dd16fe87cea25bbc4b7cba3621a1acaf310ddc2f
LR <luorui3@lenovo.com>
2021-01-23 17:25:25 +0800

    cloud native eureka build

---
+++ service-eureka/src/main/java/com/royce/serviceeureka/ServiceEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.royce.serviceeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceEurekaApplication.class, args);
+    }
+
+}
