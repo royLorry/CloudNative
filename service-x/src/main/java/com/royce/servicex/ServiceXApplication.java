37584e8872d1bace97b273c3bf0fe664d32214ff
LR <luorui3@lenovo.com>
2021-01-24 00:00:57 +0800

    caller service

--- service-x/src/main/java/com/royce/servicex/ServiceXApplication.java
+++ service-x/src/main/java/com/royce/servicex/ServiceXApplication.java
@@ -2,8 +2,10 @@ package com.royce.servicex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ServiceXApplication {
 
     public static void main(String[] args) {
--- service-x/src/main/java/com/royce/servicex/service/CountryService.java
+++ service-x/src/main/java/com/royce/servicex/service/CountryService.java
@@ -3,6 +3,8 @@ package com.royce.servicex.service;
 import com.royce.servicecommon.entity.Country;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 /**
  * 国家业务逻辑实现类
  *
@@ -13,7 +15,7 @@ import org.springframework.stereotype.Service;
 public class CountryService {
 
     public Country findCountryByName(String countryName) {
-        return new Country(1L, countryName);
+        return new Country(UUID.randomUUID().getLeastSignificantBits(), countryName);
     }
 
 }
